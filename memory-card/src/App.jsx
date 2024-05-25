6d7393171eeecb667f10268082bc5c9b7ddb955e
PhoenixTech2003 <phoenixtech2003@gmail.com>
2024-05-25 22:22:09 +0200

    Add header and style

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -1,11 +1,30 @@
 import './App.css'
 
+function Header(){
+  return(
+    <header className='bg-slate-950 text-white text-center flex flex-col items-center gap-5 p-10 sm:flex-row justify-between' >
+      <h1>MEMORY CARD GAME</h1>
+      <div className='flex flex-col gap-6 sm:flex-row'>
+        <div className='bg-slate-50 text-slate-950 px-7 py-1 rounded-lg'>
+          <p>Score : 0</p>
+        </div>
+
+        <div className='bg-slate-50 text-slate-950 px-7 py-1 rounded-lg'>
+            <p>High Score: 0</p>
+        </div>
+      </div>
+    </header>
+
+    
+  )
+}
+
 function App() {
 
 
   return (
     <>
-    
+      <Header/>
     </>
   )
 }
