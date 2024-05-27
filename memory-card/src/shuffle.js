81929febd62aa8e3f84d6fc2c0ec7d1064d2f8cf
Chiyembekezo Chilembwe <120185732+PhoenixTech2003@users.noreply.github.com>
2024-05-27 15:08:30 +0200

    Delete memory-card directory

--- memory-card/src/shuffle.js
+++
@@ -1,9 +0,0 @@
-function shuffleArray(arr) {
-  let shuffled =   arr.sort(function (a, b) {
-      return Math.random() - 0.5;
-    });
-
-    return shuffled;
-    }
-
-export default shuffleArray;
