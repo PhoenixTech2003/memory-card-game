2b30695eb9c888820ec871b52f662b42c3b91335
PhoenixTech2003 <phoenixtech2003@gmail.com>
2024-05-27 14:25:28 +0200

    Add return stament for the shuffled array

--- memory-card/src/shuffle.js
+++ memory-card/src/shuffle.js
@@ -1,7 +1,9 @@
 function shuffleArray(arr) {
-    arr.sort(function (a, b) {
+  let shuffled =   arr.sort(function (a, b) {
       return Math.random() - 0.5;
     });
+
+    return shuffled;
     }
 
 export default shuffleArray;
